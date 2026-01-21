346d4abd4cf985afe4ad211e34754312fae1a60b
Pavel Kirilin <win10@list.ru>
2026-01-21 04:58:51 +0100

    Added dynamic blend file loadings.

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,7 +1,9 @@
 extends Node3D 
 
-@export var pawn_scene: PackedScene;
+# @export var pawn_scene: PackedScene;
 @export var pawn_velocity: float = 5.0
+@export var pawns_scene: PackedScene
+@export var max_pawn_type: int = 3
 
 # Pawn currently held by a player
 var active_pawn: Pawn;
@@ -14,38 +16,63 @@ var ray_to: Vector3;
 var playing: bool = true
 var score: int = 0
 
-var items_box: Array[int] = [1, 2, 3]
+var available_pawns: Array[Node]
+var items_box: Array = []
 
 func choose_next_item() -> int:
 	var next_item = items_box.pop_back()
 	if items_box.size() == 0:
-		items_box = [1, 2, 3]
+		items_box = range(min(available_pawns.size(), max_pawn_type))
 		items_box.shuffle()
 	return next_item
 
 func spawn_new() -> void:
 	if active_pawn != null:
 		return
-	var spawn_point: Node3D = get_node("SpawnPoint") as Node3D
-	var pawn = pawn_scene.instantiate() as Pawn
-	var next_item = choose_next_item()
-	pawn.set_pawn_type(next_item)
+	var pawn_type = choose_next_item()
+	var pawn = available_pawns[pawn_type].duplicate() as Pawn
 	pawn.contact_monitor = false
+	pawn.max_pawn_type = self.available_pawns.size() - 1
 	pawn.disable_pawn_collisions()
 	pawn.freeze = true
-	pawn.position = spawn_point.position
+	pawn.set_pawn_type(pawn_type)
+	pawn.position = $SpawnPoint.position
 	pawn.connect("merged", _on_pawn_merged)
 	self.active_pawn = pawn
-	var pawns: Node3D = $Pawns
-	pawns.add_child(pawn)
-	next_item = randi_range(1, 3)
+	$SpawnedPawns.add_child(pawn)
 	$HUD.set_next_item(items_box[-1])
 
-func _on_pawn_merged(pawn_type: int) -> void:
-	score += pawn_type * 10
+func upgrade_pawn(pawn: Pawn) -> void:
+	var new_pawn_type = pawn.pawn_type + 1
+	if new_pawn_type >= available_pawns.size():
+		return
+	var new_pawn = available_pawns[new_pawn_type].duplicate() as Pawn
+	pawn.disable_pawn_collisions()
+	new_pawn.max_pawn_type = pawn.max_pawn_type
+	new_pawn.linear_velocity = pawn.linear_velocity
+	new_pawn.position = pawn.position
+	new_pawn.set_pawn_type(new_pawn_type)
+	new_pawn.connect("merged", _on_pawn_merged)
+	new_pawn.enable_pawn_collisions()
+	$SpawnPoint.add_child(new_pawn)
+	pawn.queue_free()
+
+func _on_pawn_merged(pawn: Pawn) -> void:
+	upgrade_pawn(pawn)
+	score += pawn.pawn_type * 10
 	$HUD.set_score(score)
 
+func load_pawns() -> void:
+	var loaded_scene = pawns_scene.instantiate()
+	loaded_scene.position = Vector3(0, 0, -10)
+	loaded_scene.visible = false
+	add_child(loaded_scene)
+	available_pawns = loaded_scene.get_children()
+	available_pawns.sort_custom(func(a, b): return a.name.naturalnocasecmp_to(b.name) < 0)
+	items_box = range(min(available_pawns.size(), max_pawn_type))
+
 func _ready() -> void:
+	load_pawns()
 	$Limit.connect("limit_hit", game_over)
 	items_box.shuffle()
 	$HUD.set_score(0)
@@ -63,7 +90,7 @@ func _physics_process(_delta: float) -> void:
 			return
 		var direct_state = self.get_world_3d().direct_space_state
 		var ray = PhysicsRayQueryParameters3D.create(self.ray_from,self.ray_to)
-		ray.exclude=[self.active_pawn, $Board/RightStopper, $Board/LeftStopper, $Board/ForwardStopper]
+		ray.exclude=[self.active_pawn]
 		var intersects = direct_state.intersect_ray(ray)
 		if not intersects:
 			return
--- scripts/pawn.gd
+++ scripts/pawn.gd
@@ -6,23 +6,37 @@ signal merged
 
 var velocity: Vector3 = Vector3.ZERO
 var pawn_type: int = 1;
+var blink_timer: Timer
+var max_pawn_type: int = 5;
+var mergeable: bool = false
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	self.contact_monitor = false
+	self.contact_monitor = true
 	self.max_contacts_reported = 100
 	self.axis_lock_angular_z = true
 	self.axis_lock_angular_x = true
-	self.connect("body_entered", _on_body_entered)
+	self.center_of_mass_mode = RigidBody3D.CENTER_OF_MASS_MODE_CUSTOM
+	self.center_of_mass = Vector3(0, 0.0, 0)
+	self.scale = Vector3(1,1,1)
+	$CollisionShape3D.scale = Vector3(1.1, 1.1, 1.1)
+	self.mass = 1
+	self.blink_timer = Timer.new()
+	add_child(self.blink_timer)
+
+	# self.connect("body_entered", _on_body_entered)
+	self.blink_timer.connect("timeout", _on_blink_timer_timeout)
 
 func disable_pawn_collisions():
+	self.mergeable = false
 	# Pawn layer
 	self.set_collision_layer_value(2, false)
 	# Pawn mask
 	self.set_collision_mask_value(2, false)
 
 func enable_pawn_collisions():
+	self.mergeable = true
 	# Pawn layer
 	self.set_collision_layer_value(2, true)
 	# Pawn mask
@@ -31,28 +45,30 @@ func enable_pawn_collisions():
 
 func set_pawn_type(ptype: int):
 	self.pawn_type = ptype
-	var scale_factor = 1.0 + (0.4 * self.pawn_type)
-	var scale_vector = Vector3(scale_factor, 1.0, scale_factor)
-	self.mass = ptype * 15
-	$Collider.scale = scale_vector 
-	$Mesh.scale = scale_vector 
-	$Mark.text = str(ptype)
+	self.mass = (ptype + 1) * 15
 
-func _on_body_entered(body: Node) -> void:
-	if body.get_collision_layer_value(2):
-		var pawn = body as Pawn
-		if pawn.pawn_type == self.pawn_type:
-			pawn.queue_free()
-			merged.emit(self.pawn_type)
-			self.set_pawn_type(self.pawn_type + 1)
-			self.position = self.position + (pawn.position - self.position) / 2.0
-			return
+func _physics_process(_delta: float) -> void:
+	if not mergeable:
+		return
+	if self.pawn_type == max_pawn_type:
+		return
+	for body in self.get_colliding_bodies():
+		# Other pawn entered
+		if body.get_collision_layer_value(2):
+			# If we're on a maximum pawn type, do nothing
+			var pawn = body as Pawn
+			if pawn.pawn_type == self.pawn_type and (pawn.position.z > self.position.z or pawn.position.x > self.position.x):
+				pawn.queue_free()
+				merged.emit(self)
+				self.position = self.position + (pawn.position - self.position) / 2.0
+				return
+
 
 func start_blinking() -> void:
-	$BlinkTimer.start(0.4)
+	self.blink_timer.start(0.4)
 
 func stop_blinking() -> void:
-	$BlinkTimer.stop()
+	self.blink_timer.stop()
 	self.visible = true
 
 func _on_blink_timer_timeout() -> void:
